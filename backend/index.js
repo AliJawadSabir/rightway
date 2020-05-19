43cb56c6c90e4044d5b3fba08ba844d2592a24f0
Arif Sami <arifsami03@gmail.com>
2020-05-19 19:07:24 +0500

    configured cors

--- backend/index.js
+++ backend/index.js
@@ -108,7 +108,18 @@ const PUBLIC_URLS = [
   // '/category/create',
 ];
 //cors
-app.use(cors());
+var whitelist = ['https://rightwaycollection.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 var invalidLogin = { error: true, status: 404, message: 'Invalid username or password.', 
 code: 101 };
 
